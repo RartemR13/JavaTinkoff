a3f4f637b873f81612e526d2c17fb891af74f6cc
RartemR13 <RartemR13@yandex.ru>
2023-10-08 13:34:24 +0300

    Create Task0

---
+++ src/main/java/edu/hw1/Task0.java
@@ -0,0 +1,11 @@
+package edu.hw1;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Task0 {
+    private final static Logger LOGGER = LogManager.getLogger();
+    public static void main(String[] args) {
+        LOGGER.info("Hello, world!");
+    }
+}
