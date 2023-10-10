b5483c89e6f9c6b517c6c0792f180b19699264c4
RartemR13 <RartemR13@yandex.ru>
2023-10-10 14:11:31 +0300

    solve Task1

---
+++ src/main/java/edu/hw1/Task1.java
@@ -0,0 +1,35 @@
+package edu.hw1;
+
+public final class Task1 {
+    private Task1() {
+    }
+
+    private static final int MINUTES_MAX_LEN = 5;
+    private static final int SECONDS_IN_MINUTE_MAX_COUNT = 59;
+    private static final int SECONDS_PER_MINUTE = 60;
+
+    public static int minutesToSeconds(String time) {
+        String[] timePartition = time.split(":");
+        if ((timePartition.length > 2 || timePartition.length == 0)
+            || (timePartition[0].isEmpty() || timePartition[1].length() != 2)
+            || (timePartition[0].length() > MINUTES_MAX_LEN)
+            || (timePartition[0].startsWith("0") && !(time.equals("0:00")))) {
+            return -1;
+        }
+
+        int minutes;
+        int seconds;
+        try {
+            minutes = Integer.parseInt(timePartition[0]);
+            seconds = Integer.parseInt(timePartition[1]);
+        } catch (Exception any) {
+            return -1;
+        }
+
+        if (minutes < 0 || seconds < 0 || seconds > SECONDS_IN_MINUTE_MAX_COUNT) {
+            return -1;
+        }
+
+        return minutes * SECONDS_PER_MINUTE + seconds;
+    }
+}
