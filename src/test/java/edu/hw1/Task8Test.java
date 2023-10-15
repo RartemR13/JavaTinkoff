2f39075fe5f930f0c9a071c0631b29665110fe01
RartemR13 <RartemR13@yandex.ru>
2023-10-15 20:28:21 +0300

    refact

--- src/test/java/edu/hw1/Task8Test.java
+++ src/test/java/edu/hw1/Task8Test.java
@@ -2,7 +2,6 @@ package edu.hw1;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -47,64 +46,9 @@ class Task8Test {
         );
     }
     @ParameterizedTest
+    @DisplayName("Примерные тесты")
     @MethodSource("provideBoards")
     void checkCapturer(int[][] chessBoard, boolean answer) {
         assertThat(Task8.knightBoardCapture(chessBoard)).isEqualTo(answer);
-    }
-
-
-    @Test
-    @DisplayName("Примерные тесты")
-    void checkSample() {
-        // given
-        int[][][] boards = {
-            {
-                {0, 0, 0, 1, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 1, 0, 1, 0},
-                {0, 1, 0, 0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 0, 0, 0, 1},
-                {0, 0, 0, 0, 1, 0, 0, 0}
-            },
-
-            {
-                {1, 0, 1, 0, 1, 0, 1, 0},
-                {0, 1, 0, 1, 0, 1, 0, 1},
-                {0, 0, 0, 0, 1, 0, 1, 0},
-                {0, 0, 1, 0, 0, 1, 0, 1},
-                {1, 0, 0, 0, 1, 0, 1, 0},
-                {0, 0, 0, 0, 0, 1, 0, 1},
-                {1, 0, 0, 0, 1, 0, 1, 0},
-                {0, 0, 0, 1, 0, 1, 0, 1}
-            },
-
-            {
-                {0, 0, 0, 0, 1, 0, 0, 0},
-                {0, 0, 0, 0, 0, 1, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 1, 0, 0, 0},
-                {0, 0, 0, 0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 0, 1, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0}
-            }
-        };
-
-        boolean[] correctAnswers = {true, false, false};
-
-        // when
-        boolean[] answers = new boolean[correctAnswers.length];
-        for (int i = 0; i < answers.length; ++i) {
-            try {
-                answers[i] = Task8.knightBoardCapture(boards[i]);
-            } catch (Task8.IncorrectChessBoard e) {
-                assertThat(false).isTrue();
-            }
-        }
-
-        // then
-        assertThat(answers).isEqualTo(correctAnswers);
     }
 }
