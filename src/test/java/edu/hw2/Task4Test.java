3d017348c8056d29a911403d1ce5f67367befb31
RartemR13 <RartemR13@yandex.ru>
2023-10-16 16:59:08 +0300

    solve Task 4

---
+++ src/test/java/edu/hw2/Task4Test.java
@@ -0,0 +1,18 @@
+package edu.hw2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class Task4Test {
+    private static final class CallChecker {
+        public static Task4.CallingInfo getCallingInfo() {
+            return Task4.callingInfo();
+        }
+    }
+    @Test
+    void checkSample() {
+        var callingInfo = CallChecker.getCallingInfo();
+
+        assertThat(callingInfo).isEqualTo(new Task4.CallingInfo("CallChecker", "getCallingInfo"));
+    }
+}
