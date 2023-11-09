64cc0e85bc9da2100fe10a8dcea4bbf3cf07cdb9
RartemR13 <RartemR13@yandex.ru>
2023-11-09 03:51:58 +0300

    do 4

---
+++ src/main/java/edu/hw5/Task4.java
@@ -0,0 +1,16 @@
+package edu.hw5;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class Task4 {
+    private Task4(){
+    }
+
+    public static boolean pswHaveSpecial(String psw) {
+        Pattern pattern = Pattern.compile("\\w*[~!@#$%^&*|]\\w*");
+        Matcher matcher = pattern.matcher(psw);
+
+        return matcher.matches();
+    }
+}
