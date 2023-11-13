0fae71d3c14271c07b57af08e5c8f19bc1096cd8
RartemR13 <RartemR13@yandex.ru>
2023-11-13 16:48:01 +0300

    LocalDate.parse

--- src/main/java/edu/hw5/Slash3DateHandler.java
+++ src/main/java/edu/hw5/Slash3DateHandler.java
@@ -1,25 +1,18 @@
 package edu.hw5;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @SuppressWarnings("MagicNumber")
 public final class Slash3DateHandler extends DateHandler {
     @Override
     public Optional<LocalDate> handleRequest(String request) {
-        String[] splited = request.split("/");
         Optional<LocalDate> ret;
 
-        if (splited.length != 3) {
-            return (next != null) ? next.handleRequest(request) : Optional.empty();
-        }
-
         try {
-            ret = Optional.of(LocalDate.of(
-                Integer.parseInt(splited[2]),
-                Integer.parseInt(splited[1]),
-                Integer.parseInt(splited[0])
-            ));
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/uuuu");
+            ret = Optional.of(LocalDate.parse(request, formatter));
         } catch (Exception any) {
             return (next != null) ? next.handleRequest(request) : Optional.empty();
         }
